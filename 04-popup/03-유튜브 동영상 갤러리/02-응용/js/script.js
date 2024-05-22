82d8f7c0c408fc36f270c0f478360fb8151540ad
ksb <jwe06120@gmail.com>
2024-05-22 12:10:33 +0900

    dim 추가, delay 후 동영상 삭제

--- 04-popup/03-유튜브 동영상 갤러리/02-응용/js/script.js
+++ 04-popup/03-유튜브 동영상 갤러리/02-응용/js/script.js
@@ -67,7 +67,9 @@ $(function () {
     //$videoWrap을 안 보이게
     $videoWrap.removeClass('active');
 
-    //$video의 src 값을 없애자 --> 동영상 삭제
-    $video.attr('src', '');
+    //$video의 src 값을 없애자 --> 0.3초 후 동영상 삭제
+    setTimeout(function () {
+      $video.attr('src', '');
+    }, 300);
   });
 });

cc387fdcee5364cd25b19ba3ed76d1e454f7f88c
ksb <jwe06120@gmail.com>
2024-06-13 11:19:35 +0900

    움직임의 범위 설정 Math.min(), Math.max()

--- 08-GASP/실습예제/No-Plan-B/js/script.js
+++ 08-GASP/실습예제/No-Plan-B/js/script.js
@@ -90,6 +90,7 @@ $(function () {
     // console.log(e);
     // Math.min(a,b) Math.max(a,b)
     x = Math.max(-100, Math.min(200, e.pageX - $window.innerWidth() / 2));
+    y = Math.max(-10, Math.min(100, e.pageY - $window.innerHeight() / 2));
 
     // 전체 화면크기1920의 절반960을 빼줌 =>정중앙 기준으로 만들기(화면 중심에 마우스 중심을 맞춤)
     // x = e.pageX - $window.innerWidth() / 2;
@@ -98,13 +99,14 @@ $(function () {
   //대상(이소룡)을 움직이게 하는 함수
   function moving() {
     mx += (x - mx) * speed;
-    // my += (y - my) * speed;
+    my += (y - my) * speed;
     $('.bruce-lee').css({
       //transform: `translate(${mx}px, ${my}px)`
       transform: `translateX(${mx}px)`,
     });
     $('.bruce-lee-bg').css({
-      transform: `translateX(${mx * 0.4}px)`,
+      transform: `translate(${mx * 0.4}px, ${my}px)`,
+      filter: `blur(${mx * 0.05}px)`,
     });
     $('.title').css({
       transform: `translate(${-mx * 0.4}px, -50%)`,
